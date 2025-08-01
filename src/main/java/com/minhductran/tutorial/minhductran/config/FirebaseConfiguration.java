05dcbe70059e61ea5584f74a0eefb27920b26125
minhductran042@gmail.com <minhductran042@gmail.com>
2025-08-01 10:06:44 +0700

    feat: Cấu hình FireBase để gửi nottification push

---
+++ src/main/java/com/minhductran/tutorial/minhductran/config/FirebaseConfiguration.java
@@ -0,0 +1,30 @@
+package com.minhductran.tutorial.minhductran.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Configuration
+public class FirebaseConfiguration {
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException { // Khởi tạo FirebaseApp
+        InputStream serviceAccount = new ClassPathResource("notification-push-firebase.json").getInputStream();
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        return FirebaseApp.initializeApp(options);
+    }
+
+    @Bean
+    FirebaseMessaging firebaseMessaging(FirebaseApp firebaseApp) {
+        return FirebaseMessaging.getInstance(firebaseApp);
+    } // Trả về FirebaseMessaging instance
+}
